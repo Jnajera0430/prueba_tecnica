cf221f8c06583adf950d703ddfea8d6378379583
Jnajera0430 <najerajoseantonio052@gmail.com>
2023-10-29 12:19:06 -0500

    fix: se cambió objeto de errores

--- src/presentation/controllers/user/createUser.controller.ts
+++ src/presentation/controllers/user/createUser.controller.ts
@@ -3,12 +3,13 @@ import { CreateUserUseCase } from "../../../domain/interfaces/useCases/user/crea
 import { CreateUserDto } from "../../../domain/dto/user.dto";
 import { validate } from 'class-validator';
 import { plainToClass } from "class-transformer";
+import UserModel from "../../../data/dataSources/sequalize/models/userModel";
 
 export const postCreateUser = async (req: Request, res: Response, createUserUseCase: CreateUserUseCase) => {
     try {
         const createUserDto = plainToClass(CreateUserDto, req.body);
         const errors = await validate(createUserDto);
-        
+
         if (errors.length > 0) {
             return res.status(400).json({
                 error: 'Validation failed',
@@ -16,6 +17,18 @@ export const postCreateUser = async (req: Request, res: Response, createUserUseC
                     field: error.property,
                     constraints: error.constraints,
                 })),
+            });
+        }
+        const userFound = await UserModel.findOne({
+            where: {
+                email: createUserDto.email
+            }
+        })
+
+        if (userFound) {
+            return res.status(400).json({
+                error: 'Validation failed',
+                details: [{ field: 'email', constrains: { isEmail: 'email must be unique' } }],
             });
         }
         await createUserUseCase.execute(createUserDto);
