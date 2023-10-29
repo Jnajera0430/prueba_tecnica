85c00f65a7309e728da72edf641abee893054624
Jnajera0430 <najerajoseantonio052@gmail.com>
2023-10-28 22:33:11 -0500

    server listo

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 import express from 'express';
+import cors from 'cors'
 const server = express();
 //config
 server.use(express.json());
+server.use(cors({
+    origin: "*"
+}))
 
 export default server
